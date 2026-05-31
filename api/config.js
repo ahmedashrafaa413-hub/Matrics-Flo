5d6ef38ebb0051442e02996f2dc592b573df3dee
ahmedashrafaa413-hub <ahmed.ashraf.aa413@gmail.com>
2026-06-01 01:06:04 +0300

    Delete api directory

--- api/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (req, res) => {
-  res.status(200).json({
-    success: true,
-    meta_app_id_exists: !!process.env.META_APP_ID,
-    meta_app_secret_exists: !!process.env.META_APP_SECRET
-  });
-};
