79500dcc05740d20976fc4d6681fa09a96144f48
ahmedashrafaa413-hub <ahmed.ashraf.aa413@gmail.com>
2026-06-19 15:35:41 +0300

    Update route.js

--- app/api/intelligence/analyze/route.js
+++ app/api/intelligence/analyze/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import { getSnapchatToken } from "../../../../../lib/snapchatToken";
+import { getSnapchatToken } from "../../../../lib/snapchatToken";
 
 export const dynamic = "force-dynamic";
 
